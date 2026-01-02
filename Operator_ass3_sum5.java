ce422eb309b7766dcd1303629395d0c7823cf235
rithikaselvaraj15 <rithikaselvaraj22@gmail.com>
2026-01-02 10:47:43 +0530

    Add files via upload

---
+++ Operator_ass3_sum5.java
@@ -0,0 +1,29 @@
+
+// 5.Write a program to swap two numbers without using a third variable.
+
+package com.index;
+
+public class Operator_ass3_sum5 {
+
+	public static void main(String[] args) {
+		
+	
+
+		        int a = 10;
+		        int b = 20;
+
+		       
+		        a = a + b;
+		        b = a - b;
+		        a = a - b;
+
+		        System.out.println("After swapping:");
+		        System.out.println("a = " + a);
+		        System.out.println("b = " + b);
+		    }
+		
+
+	}
+
+}
+
